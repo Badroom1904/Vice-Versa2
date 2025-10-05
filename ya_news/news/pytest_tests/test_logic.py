90672e96037a51b2602368eced1c51240c17e7ae
Alex <Magnatov2020@mail.ru>
2025-10-05 20:34:29 +0300

    Try

--- ya_news/news/pytest_tests/test_logic.py
+++ ya_news/news/pytest_tests/test_logic.py
@@ -38,7 +38,7 @@ def test_user_cant_use_bad_words(author_client, news):
     bad_words_data = {'text': f'Какой-то текст, {BAD_WORDS[0]}, еще текст'}
     url = reverse('news:detail', args=(news.id,))
     response = author_client.post(url, data=bad_words_data)
-    assertFormError(response, errors=WARNING, field='text', form='form')
+    assertFormError(response, 'form', 'text', WARNING)
     comments_count = Comment.objects.count()
     assert comments_count == 0
 
